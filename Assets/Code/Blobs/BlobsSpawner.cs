8c644fc44084100851e8d45a88c4514e165c1780
Sergey Ankinovich <codeavr@ya.ru>
2020-07-10 21:17:47 +0300

    Added non collision position search on blobs spawner

--- Assets/Code/Blobs/BlobsSpawner.cs
+++ Assets/Code/Blobs/BlobsSpawner.cs
@@ -4,7 +4,11 @@ using RedsAndBlues.ECS.PhysicsEngine;
 using RedsAndBlues.ECS.PhysicsEngine.Components;
 using RedsAndBlues.ECS.PhysicsEngine.Tags;
 using RedsAndBlues.ECS.Rendering;
+using RedsAndBlues.Utils;
+using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
+using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
@@ -14,13 +18,17 @@ namespace RedsAndBlues.Blobs
 {
     public class BlobsSpawner
     {
+        private const int MaxFindPositionAttempts = 32;
         private EntityArchetype _ballArchetype;
         private EntityManager _manager;
         private int _spawnedCount;
+        private EntityQuery _circlesQuery;
 
         public BlobsSpawner(EntityManager manager)
         {
             _manager = manager;
+            _circlesQuery = _manager.CreateEntityQuery(typeof(CircleColliderComponent), typeof(Translation));
+
             _ballArchetype = _manager.CreateArchetype
             (
                 typeof(Translation),
@@ -39,12 +47,14 @@ namespace RedsAndBlues.Blobs
             var speed = Random.Range(spawnSettings.MinUnitSpeed, spawnSettings.MaxUnitSpeed);
             var radius = Random.Range(spawnSettings.MinUnitRadius, spawnSettings.MaxUnitRadius);
             bool isRed = _spawnedCount % 2 == 0;
-            var position = new float3
+
+            var area = new float2
             (
-                Random.Range(-.5f, .5f) * (spawnSettings.GameAreaSettings.Width - radius * 2),
-                Random.Range(-.5f, .5f) * (spawnSettings.GameAreaSettings.Height - radius * 2),
-                spawnSettings.ZPosition
+                spawnSettings.GameAreaSettings.Width - radius * 2,
+                spawnSettings.GameAreaSettings.Height - radius * 2
             );
+
+            var position = FindSuitablePosition(area, radius) + new float3(0, 0, spawnSettings.ZPosition);
 
             var entity = _manager.CreateEntity(_ballArchetype);
 
@@ -81,6 +91,88 @@ namespace RedsAndBlues.Blobs
         {
             var query = _manager.CreateEntityQuery(typeof(BlobPropertiesComponent));
             _manager.AddComponent(query, typeof(IsMovingTag));
+        }
+
+        private float3 FindSuitablePosition(float2 area, float circleRadius)
+        {
+            var entities = _circlesQuery.ToEntityArray(Allocator.TempJob);
+            var colliders = _circlesQuery.ToComponentDataArray<CircleColliderComponent>(Allocator.TempJob);
+            var translations = _circlesQuery.ToComponentDataArray<Translation>(Allocator.TempJob);
+            NativeArray<bool> isSuitable = new NativeArray<bool>(1, Allocator.TempJob);
+
+            float3 position = default;
+
+            for (int i = 0; i < MaxFindPositionAttempts; i++)
+            {
+                position = new float3(Random.Range(-.5f, .5f) * area.x, Random.Range(-.5f, .5f) * area.y, 0);
+
+                isSuitable[0] = true;
+
+                var job = new FindSuitablePositionJob
+                {
+                    Colliders = colliders,
+                    Translations = translations,
+                    Position = position,
+                    Radius = circleRadius,
+                    CollisionError = circleRadius * .1f,
+                    IsSuitable = isSuitable
+                };
+
+                var jobHandle = job.Schedule();
+
+                jobHandle.Complete();
+
+                if (job.IsSuitable[0])
+                {
+                    break;
+                }
+            }
+
+            entities.Dispose();
+            colliders.Dispose();
+            translations.Dispose();
+            isSuitable.Dispose();
+
+            return position;
+        }
+
+        [BurstCompile]
+        private struct FindSuitablePositionJob : IJob
+        {
+            [ReadOnly]
+            public NativeArray<CircleColliderComponent> Colliders;
+
+            [ReadOnly]
+            public NativeArray<Translation> Translations;
+
+            [ReadOnly]
+            public float3 Position;
+
+            [ReadOnly]
+            public float Radius;
+
+            [ReadOnly]
+            public float CollisionError;
+
+            [WriteOnly]
+            public NativeArray<bool> IsSuitable;
+
+            public void Execute()
+            {
+                for (int index = 0; index < Colliders.Length; index++)
+                {
+                    if (PhysicsUtility.DoCirclesOverlap
+                    (
+                        Position, Radius,
+                        Translations[index].Value, Colliders[index].Radius,
+                        CollisionError, out _
+                    ))
+                    {
+                        IsSuitable[0] = false;
+                        return;
+                    }
+                }
+            }
         }
     }
 }
